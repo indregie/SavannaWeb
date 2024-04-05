e01a563d286d90e1a2c2012d5035377440125777
Indre Giedraityte <indre.giedraityte@accenture.com>
2024-04-05 18:56:40 +0300

    Db added, Account controller added, ViewModels added

--- Savanna.Frontend/Program.cs
+++ Savanna.Frontend/Program.cs
@@ -1,10 +1,34 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Savanna.Backend;
 using Savanna.Frontend;
+using Savanna.Frontend.Data;
+using Savanna.Frontend.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
+var connectionString = builder.Configuration.GetConnectionString("default");
+
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<AppDbContext>(
+    options => options.UseSqlServer(connectionString)
+    );
+
+//identity to be able to use EF
+builder.Services.AddIdentity<AppUser, IdentityRole>(
+    options =>
+    {
+        options.Password.RequiredUniqueChars = 0;
+        options.Password.RequireUppercase = false;
+        options.Password.RequiredLength = 4;
+        options.Password.RequireNonAlphanumeric = false;
+        options.Password.RequireLowercase = false;
+    }
+    )
+    .AddEntityFrameworkStores<AppDbContext>()
+    .AddDefaultTokenProviders();
 
 builder.Services.AddSingleton<BoardManager>();
 builder.Services.AddSingleton<UIManager>();
