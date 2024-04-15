4fe9fdfeea0fc777917cb6e4dea42a469ef4d812
Indre Giedraityte <indre.giedraityte@accenture.com>
2024-04-15 10:38:36 +0300

    Game model fixed

--- Savanna.Frontend/Models/Game.cs
+++ Savanna.Frontend/Models/Game.cs
@@ -1,7 +1,13 @@
-﻿namespace Savanna.Frontend.Models;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Savanna.Frontend.Models;
 
 public class Game
 {
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int Id { get; set; }
     public string? UserId { get; set; }
     public string? AnimalsJson { get; set; }
 }
